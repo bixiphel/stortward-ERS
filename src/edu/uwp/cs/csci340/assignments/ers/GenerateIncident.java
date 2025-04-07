3ed8d7928430860ef90aa3dd9712ff1160a4804a
bixiphel <jacka10peil123@gmail.com>
2025-04-07 12:42:24 -0500

    Added functionality to create a CSV file.

--- src/edu/uwp/cs/csci340/assignments/ers/GenerateIncident.java
+++ src/edu/uwp/cs/csci340/assignments/ers/GenerateIncident.java
@@ -1,4 +1,20 @@
 package edu.uwp.cs.csci340.assignments.ers;
 
+import java.io.File;
+import java.io.IOException;
+
 public class GenerateIncident {
+    // This generates incidents randomly and stores it in a CSV file
+    public static void main(String[] args) {
+        try {
+            File incidents = new File("incidents.csv");
+            if(incidents.createNewFile()) {
+                System.out.println("File created");
+            } else {
+                System.out.println("File already exists");
+            }
+        } catch (IOException e) {
+            System.out.println("File could not be created.");
+        }
+    }
 }
